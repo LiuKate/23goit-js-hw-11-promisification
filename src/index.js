7160b5de7fe4e4dd7cee773a2bd23c40e1a80f68
Kateryna Liubchyk <shevtsova.kateryna@gmail.com>
2020-11-22 13:35:45 +0200

    добавляет решение задачи 3

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ const users = [
 const toggleUserState = (allUsers, userName) => {
 
  return new Promise(() => {
-        // if(!allUsers) reject(Error("Userlist is invalid"));
+        
         const updatedUsers = allUsers.map(user =>
             user.name === userName ? { ...user, active: !user.active } : user,
           );
@@ -46,3 +46,65 @@ const logger = updatedUsers => console.table(updatedUsers);
  */
 toggleUserState(users, 'Mango').then(logger);
 toggleUserState(users, 'Lux').then(logger);
+
+
+
+
+// Задание 3
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const makeTransaction = (transaction) => {
+  const delay = randomIntegerFromInterval(200, 500);
+
+    return new Promise((resolve, reject) => {
+
+      setTimeout(() => {
+
+        const canProcess = Math.random() > 0.3;
+
+      if (canProcess) {
+        resolve({id:transaction.id, time:delay});
+      } else {
+        reject(transaction.id);
+      }
+     
+    }, delay);
+  })
+};
+
+const logSuccess = ({id, time}) => {
+  console.log(`Transaction ${id} processed in ${time}ms`);
+};
+
+const logError = id => {
+  console.warn(`Error processing transaction ${id}. Please try again later.`);
+};
+
+/*
+ * Работает так
+ */
+// makeTransaction({ id: 70, amount: 150 }, logSuccess, logError);
+// makeTransaction({ id: 71, amount: 230 }, logSuccess, logError);
+// makeTransaction({ id: 72, amount: 75 }, logSuccess, logError);
+// makeTransaction({ id: 73, amount: 100 }, logSuccess, logError);
+/*
+ * Должно работать так
+ */
+makeTransaction({ id: 70, amount: 150 })
+  .then(logSuccess)
+  .catch(logError);
+
+makeTransaction({ id: 71, amount: 230 })
+  .then(logSuccess)
+  .catch(logError);
+
+makeTransaction({ id: 72, amount: 75 })
+  .then(logSuccess)
+  .catch(logError);
+
+makeTransaction({ id: 73, amount: 100 })
+  .then(logSuccess)
+  .catch(logError);
